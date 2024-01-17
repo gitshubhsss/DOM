35f4ee1eac760ab85be435c88d66d9898def4d42
gitshubhsss <shubhamranjane16@gmail.com>
2024-01-17 12:35:37 +0530

    querySelectorAll

---
+++ querySel.js
@@ -0,0 +1,4 @@
+const x= document.querySelectorAll("p.intro");//here x we use for accessing the index
+
+document.getElementById("demo").innerHTML="this is under demo and we are using querySelectorAll for accesscing the main class "+x[1].innerHTML;
+
