69e7c69561cc894b08c213a44cd5d54bd1b73b4d
gitshubhsss <shubhamranjane16@gmail.com>
2024-01-17 11:59:32 +0530

    get elements by className

---
+++ getEleByClass.js
@@ -0,0 +1,3 @@
+const x=document.getElementsByClassName("main");//x will help in indexing
+
+document.getElementById("demo").innerHTML="This is the example of Getting the elements by class  the first element of class main is "+x[0].innerHTML;
