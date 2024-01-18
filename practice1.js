9ef8b89a1a78c2478bcc136bf727084e25c73999
gitshubhsss <shubhamranjane16@gmail.com>
2024-01-18 17:03:09 +0530

    practice question1

---
+++ practice1.js
@@ -0,0 +1,7 @@
+let add_btn=document.createElement("button");
+add_btn.innerText="click me";
+add_btn.style.color="white";
+add_btn.style.backgroundColor="red";
+
+let btn=document.querySelector("body").prepend(add_btn);
+
